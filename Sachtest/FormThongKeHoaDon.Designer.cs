5f6f083de2393d656b4aa20c17469834ed07b63c
Minurte1 <hohoangphucjob@gmail.com>
2024-01-03 18:37:40 +0700

    cap nhat

--- Sachtest/FormThongKeHoaDon.Designer.cs
+++ Sachtest/FormThongKeHoaDon.Designer.cs
@@ -35,11 +35,11 @@
             // dataGridView1
             // 
             this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
-            this.dataGridView1.Location = new System.Drawing.Point(12, 6);
+            this.dataGridView1.Location = new System.Drawing.Point(1, 1);
             this.dataGridView1.Name = "dataGridView1";
             this.dataGridView1.RowHeadersWidth = 51;
             this.dataGridView1.RowTemplate.Height = 24;
-            this.dataGridView1.Size = new System.Drawing.Size(1748, 612);
+            this.dataGridView1.Size = new System.Drawing.Size(1280, 622);
             this.dataGridView1.TabIndex = 0;
             this.dataGridView1.CellContentClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dataGridView1_CellContentClick);
             // 
